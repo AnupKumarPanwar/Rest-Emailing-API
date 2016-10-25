f5b90c0558c4e1275a83f11e92ddbb52f6946cbe
AnupKumarPanwar <1anuppanwar@gmail.com>
2016-10-25 20:56:53 +0530

    New

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ var smtpTransport = nodemailer.createTransport("SMTP",{
     service: "Gmail",
     auth: {
         XOAuth2: {
-	      user: "youremail@gmail.com", // Your gmail address.
-	       pass: "yourpassword",                                     // Not @developer.gserviceaccount.com
-	      clientId: "496112663784-227c9tum8bjdtdp74duu0p4195h8bdng.apps.googleusercontent.com",
-	      clientSecret: "qHHApUumnhM9Aqir7OZTVNTC",
-	      refreshToken: "1/bGiuPq1orYXhmWSSvRI-w6miHy6G3YNAOrGVsemMC3c"
+	      user: "username@gmail.com", // Your gmail address.
+	       pass: "XXXXXXXXXXXXXXXX",                                     // Not @developer.gserviceaccount.com
+	      clientId: "XXXXXX-XXXXXXX-XXXXX.apps.googleusercontent.com",
+	      clientSecret: "XXXXXXXXXXXXXXXX",
+	      refreshToken: "XXXXXXXXXXXXXXXX"
 	    }
     }
 });
@@ -31,8 +31,8 @@ var smtpTransport = nodemailer.createTransport("SMTP",{
 app.get('/:to/:text',function(req,res){
     var mailOptions={
         to : req.params.to,
-        subject : 'Youth Canvas Verification Email',
-        html : 'Hi '+req.params.to+',<br>&nbsp; &nbsp; &nbsp; Welcome to Youth Canvas Family. Your Verification Code is <b>'+req.params.text + ' </b>. <br><br>Thank You<br>Have a Good Day'
+        subject : 'Subject',
+        html : 'Hi '+req.params.to+',<br>&nbsp; &nbsp; &nbsp; Welcome to Nodemailer . Message <b>'+req.params.text + ' </b>. <br><br>Thank You<br>Have a Good Day'
     }
     console.log(mailOptions);
     smtpTransport.sendMail(mailOptions, function(error, response){
